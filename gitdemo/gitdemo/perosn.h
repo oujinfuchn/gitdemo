63b3be6d9446e5405c0f02938aefa9110a326b33
oujinfu <oujinfuchn@163.com>
2017-01-17 21:53:40 +0800

    添加person类

---
+++ gitdemo/gitdemo/perosn.h
@@ -0,0 +1,13 @@
+//
+//  perosn.h
+//  gitdemo
+//
+//  Created by Edison on 17/1/17.
+//  Copyright © 2017年 Edison. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface perosn : NSObject
+
+@end
---
+++ gitdemo/gitdemo/perosn.m
@@ -0,0 +1,13 @@
+//
+//  perosn.m
+//  gitdemo
+//
+//  Created by Edison on 17/1/17.
+//  Copyright © 2017年 Edison. All rights reserved.
+//
+
+#import "perosn.h"
+
+@implementation perosn
+
+@end
